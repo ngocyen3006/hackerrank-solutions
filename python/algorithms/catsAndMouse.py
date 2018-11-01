b6d93b33ef11aed8c4ab10a3c652d851683113c4
Yen Nguyen <ngocyen300693@gmail.com>
2018-11-01 11:13:19 +0700

    Solve "Cats and a Mouse" on hackerrank

---
+++ python/algorithms/catsAndMouse.py
@@ -0,0 +1,23 @@
+# https://www.hackerrank.com/challenges/cats-and-a-mouse/problem
+
+def catAndMouse(x, y, z):
+    AC = abs(z - x)
+    BC = abs(z - y)
+    if AC == BC:
+        return "Mouse C"
+    if AC < BC:
+        return "Cat A"
+    return "Cat B"
+
+
+if __name__ == '__main__':
+    q = int(input())
+
+    for q_itr in range(q):
+        xyz = input().split()
+        x = int(xyz[0])
+        y = int(xyz[1])
+        z = int(xyz[2])
+
+        result = catAndMouse(x, y, z)
+        print(result)
