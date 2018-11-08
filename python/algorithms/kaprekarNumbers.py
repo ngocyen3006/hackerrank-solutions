67eaa436df13b935c233a0e13ad33b5c5c8c9414
Yen Nguyen <ngocyen300693@gmail.com>
2018-11-08 23:24:36 +0700

    Improve "Modified Kaprekar Numbers" solution on hackerrank

--- python/algorithms/kaprekarNumbers.py
+++ python/algorithms/kaprekarNumbers.py
@@ -2,29 +2,25 @@
 
 def kaprekarNumbers(p, q):
     var = False
+    if 1 in range(p,q+1):
+        print(1,end=" ")
+        var = True
+    p = max(p,9)
     for i in range(p, q + 1):
         print(end="")
         square = i ** 2
         s = str(square)
-        digit = len(s)
-        if digit == 1:
-            if square == i:
-                print(i, end=" ")
-                var = True
-        else:
-            digit = digit // 2
-            l = s[:digit]
-            r = s[digit:]
-            sum = int(l) + int(r)
-            if sum == i:
-                print(i, end=" ")
-                var = True
+        digit = len(s) // 2
+        sum = int(s[:digit]) + int(s[digit:])
+        if sum == i:
+            print(i, end=" ")
+            var = True
     if var == False:
         print("INVALID RANGE")
 
 
 if __name__ == '__main__':
     p = 1
-    q = 300
+    q = 1000
 
     kaprekarNumbers(p, q)
