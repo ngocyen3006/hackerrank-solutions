266cdee3f4d40cd0917a98a08fd21f692bc9e770
Yen Nguyen <ngocyen300693@gmail.com>
2018-11-08 16:05:52 +0700

    Solve "Dynamic Array" on hackerrank

---
+++ python/data-structure/dynamicArray.py
@@ -0,0 +1,31 @@
+# https://www.hackerrank.com/challenges/dynamic-array/problem
+
+def dynamicArray(n, queries):
+    s = [[] * n for _ in range(n)]
+    lastAnswer = 0
+    for i in queries:
+        if i[0] == 1:
+            index = (i[1] ^ lastAnswer) % n
+            s[index].append(i[2])
+            continue
+        if i[0] == 2:
+            index1 = (i[1] ^ lastAnswer) % n
+            index2 = i[2] % len(s[index1])
+            lastAnswer = s[index1][index2]
+            print(lastAnswer)
+    pass
+
+
+if __name__ == '__main__':
+    queries = []
+    with open("testCase.txt") as file:
+        line = file.readline()
+        nq = line.rstrip().split()
+        n = int(nq[0])
+        q = int(nq[1])
+        line = file.readline()
+        while line:
+            queries.append(list(map(int, line.rstrip().split())))
+            line = file.readline()
+
+    dynamicArray(n, queries)
